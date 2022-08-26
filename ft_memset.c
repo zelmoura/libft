21bb8a30cd14b4e8af7eef01861f9e72cc6dcb05
zohair99 <zohirmourabit@gmail.com>
2022-08-26 19:54:29 +0100

    add ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,15 @@
+void	*ft_memset(void *s, int c, size_t n)
+{
+	while (n--)
+		*(char *)(s + n) = (char)c;
+	return (s);
+}
+/*
+int main()
+{
+    char p[] = "hilali&Mourabit";
+    ft_memset(p, 'k', 5);
+    printf("%s", p);
+   return(0);
+   
+}*/
